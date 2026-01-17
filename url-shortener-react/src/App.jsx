2b8cead9ec23f8bafcb0cd16ef9dd6a4ddb38f8d
kartikgupta <kartik.gupta2505@gmail.com>
2026-01-17 11:47:58 +0530

    added langing page, about page and styling

--- url-shortener-react/src/App.jsx
+++ url-shortener-react/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import LandingPage from './components/LandingPage.jsx'
+import AboutPage from './components/AboutPage.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <Router>
+    <Navbar />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/about' element={<AboutPage />} />
+      </Routes>
+    <Footer />
+    </Router>
   )
 }
 export default App
